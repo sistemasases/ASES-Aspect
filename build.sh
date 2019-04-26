9617341af6cee586da67340b3579852538931553
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2019-04-26 11:30:27 -0500

    Added debug message

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ rm build/*.css
 rm build/*.map
 sass scss/aaspect.scss build/aaspect.css 
 sass scss/aaspect.scss build/aaspect.min.css --style compressed 
+uglifyjs --compress --mangle -- js/aaspect.js
 uglifyjs --compress --mangle -- js/aaspect.js > build/aaspect.min.js
