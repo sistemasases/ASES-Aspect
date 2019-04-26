d30981fe73d7a80055cadfa05f403654ce895f97
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2019-04-26 10:23:41 -0500

    Tab base

--- js/aaspect.js
+++ js/aaspect.js
@@ -3,6 +3,15 @@
  * Licensed under the MIT license
  */
 
-if (typeof jQuery === 'undefined') {
-  throw new Error('AAspect\'s JavaScript requires jQuery')
-}
+ if (typeof jQuery === 'undefined') {
+ 	throw new Error('AAspect\'s JavaScript requires jQuery')
+ }
+
+//Tab pane control
++function($){
+	'use strict';
+	var Tab = function(element){
+		this.element = $(element);
+	}
+
+}(jQuery);
